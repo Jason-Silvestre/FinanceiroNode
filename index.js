ed9ab216e4cc3abf7b9aaae9484445a00fc89977
FABRICA01 <jasonsilvestre43@yahoo.com>
2021-01-23 00:36:37 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const MercadoPago = require("mercadopago");
+const app = express();
+
+MercadoPago.configure({
+    sandbox: true,
+    access_token:"TEST-8069843486273788-012219-d8b9c8aac4ab8a3f4c5b3a925687104c-148888633"
+});
+
+app.get("/", (req, res) => {
+    res.send("Olá mundo!" + Date.now());
+});
+
+app.get("/pagar",async (req, res) => {
+
+
+var id = "" + Date.now();
+var emailDoPagador = "jasonsilvestre34@gmail.com";
+    var dados = {
+        items: [
+            item = {
+                //UUID && Data
+                id: id,
+                title: "2 bíblias",
+                quantity: 1,
+                currency_id: 'BRL',
+                unit_price: parseFloat(150)
+            }
+        ],
+        payer:{
+           email: emailDoPagador
+        },
+        external_reference:id,
+    }
+
+    try{
+        var pagamento = await MercadoPago.preferences.create(dados);
+        console.log(pagamento);
+        //Banco.SalvarPagamento({id: id, pagador: email});
+        return res.redirect(pagamento.body.init_point);
+    }catch(err){
+        return res.send(err.message);
+    }    
+});
+
+app.listen(3000,(req, res) => {
+    console.log("Servidor rodando!");
+    
+});
